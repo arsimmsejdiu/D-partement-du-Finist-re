0c54351a90e14669afa985e437cfa7f0e9afb952
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-04-16 11:51:00 +0200

    added the service section and an image gallery

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,48 +1,38 @@
 import React from "react";
 import Tittle from "../Components/Tittle";
-import ImageSection from "../Components/ImageSection";
+import ImageGallery from "../Components/ImageGallery";
 //import SkillsSection from "../Components/SkillsSection";
 import ServicesSection from "../Components/ServicesSection";
-import design from "../img/design.svg";
-import intelligence from "../img/intelligence.svg";
-import gamedev from "../img/game-dev.svg";
+import heart from "../img/heart.png";
+import backpack from "../img/backpack.png";
+import familly from "../img/family.png";
+import mask from '../img/masks.png';
 
 function AboutPage() {
   return (
     <div className="AboutPage">
       <Tittle title={"Les lieux incontournables"} span={"Les lieux incontournables"} />
-      <ImageSection />
+      <ImageGallery />
       <Tittle title={"Vos services"} span={"Vos services"} />
       <div className="servives-container">
         <ServicesSection
-          image={design}
-          title={"Web design"}
+          image={heart}
+          title={"Action Social / CDAS"}
           text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
         />
         <ServicesSection
-          image={intelligence}
-          title={"Artificial Intelligence"}
+          image={backpack}
+          title={"Collèges"}
           text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
         />
         <ServicesSection
-          image={gamedev}
-          title={"Game Development"}
-          text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-        />
-
-        <ServicesSection
-          image={design}
-          title={"Web design"}
+          image={familly}
+          title={"Enfance / Famille"}
           text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
         />
         <ServicesSection
-          image={intelligence}
-          title={"Artificial Intelligence"}
-          text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
-        />
-        <ServicesSection
-          image={gamedev}
-          title={"Game Development"}
+          image={mask}
+          title={"Culture"}
           text={"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
         />
       </div>
@@ -51,17 +41,3 @@ function AboutPage() {
 }
 
 export default AboutPage;
-
-// eslint-disable-next-line no-lone-blocks
-{/* <Tittle title={"My Skills"} span={"My Skills"} />
-      <div className="skillsContainer">
-        <SkillsSection skill={"Javascript"} progress={"60%"} width={"60%"} />
-        <SkillsSection skill={"Typescript"} progress={"30%"} width={"30%"} />
-        <SkillsSection skill={"React Js"} progress={"70%"} width={"70%"} />
-        <SkillsSection skill={"Node Js"} progress={"80%"} width={"80%"} />
-        <SkillsSection skill={"Python"} progress={"90%"} width={"90%"} />
-        <SkillsSection skill={"Lua"} progress={"40%"} width={"40%"} />
-        <SkillsSection skill={"Java"} progress={"80%"} width={"80%"} />
-        <SkillsSection skill={"Web Design"} progress={"25%"} width={"25%"} />
-        <SkillsSection skill={"UI/Ux Design"} progress={"76%"} width={"76%"} />
-      </div> */}
